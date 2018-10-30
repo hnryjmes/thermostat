a42a8a5abd857e3d42022745c6e4c33f843f1805
becc-mu <becc.mulugeta@gmail.com>
2018-10-30 15:26:40 +0000

    set power saving mode to default

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,13 @@
 function Thermostat() {
   this.temp = 20;
   this.MIN_TEMP = 10;
-  this.MAX_TEMP = 25;
+  this.maxTemp = 25;
+  this.powerSave = true;
 }
 
 Thermostat.prototype.up = function() {
-  if (this.temp >= this.MAX_TEMP) {
-    this.temp = this.MAX_TEMP;
+  if (this.temp >= this.maxTemp) {
+    this.temp = this.maxTemp;
   } else {
     this.temp++;
   }};
@@ -16,5 +17,15 @@ Thermostat.prototype.down = function() {
     this.temp = this.MIN_TEMP;
   } else {
     this.temp--;
+  }
+};
+
+Thermostat.prototype.togglePowerSave = function() {
+  if(this.powerSave) {
+    this.maxTemp = 32;
+    this.powerSave = false;
+  } else {
+    this.maxTemp = 25;
+    this.powerSave = true;
   }
 };
