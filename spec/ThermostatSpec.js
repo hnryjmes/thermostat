aa21c854c29445cf4228ce711a6e271e2dbb6f58
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-30 15:10:45 +0000

    min temp and max temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,12 +5,31 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
-  it('should start temperature at 20 degrees', function() {
+  it('should start with temperature at 20 degrees', function() {
     expect(thermostat.temp).toEqual(20);
   });
 
   it('should be able to increase temperature', function() {
     thermostat.up();
     expect(thermostat.temp).toEqual(21);
+  });
+
+  it('should be able to decrease temperature', function() {
+    thermostat.down();
+    expect(thermostat.temp).toEqual(19);
+  });
+
+  it('should not set temperature below 10', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.temp).toEqual(10);
+  });
+
+  it('should not set temperature above 25', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.temp).toEqual(25);
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,8 +1,20 @@
 function Thermostat() {
   this.temp = 20;
-
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP = 25;
 }
 
 Thermostat.prototype.up = function() {
-  this.temp++;
+  if (this.temp >= this.MAX_TEMP) {
+    this.temp = this.MAX_TEMP;
+  } else {
+    this.temp++;
+  }};
+
+Thermostat.prototype.down = function() {
+  if (this.temp <= this.MIN_TEMP) {
+    this.temp = this.MIN_TEMP;
+  } else {
+    this.temp--;
+  }
 };
