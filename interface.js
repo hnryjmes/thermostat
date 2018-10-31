f4030ee28726e8aa4845759384daedae9e971f38
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-31 15:54:38 +0000

    add api call

--- interface.js
+++ interface.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
 var thermostat = new Thermostat();
+const API_KEY = '53c595b5439bec609a86dee8177fd19f';
+const API_CALL = `http://api.openweathermap.org/data/2.5/weather?q={London}&units=metric&APPID={${API_KEY}}`;
+
+const someJSON = '{"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}';
 
 $('#temperature').text( thermostat.temperature )
 
@@ -26,5 +30,11 @@ $( "#powerSavingModeOn" ).click(function( event ) {
 $( "#powerSavingModeOff" ).click(function( event ) {
   thermostat.switchPowerSavingModeOff();
 });
+//
+// $.get(API_CALL, function(response) {
+//   $('#outsideWeather').text( response.main.temp );
+// });
+
+$('#outsideWeather').text( JSON.parse(someJSON).main.temp );
 
 });
